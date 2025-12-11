379eefbbecc5c817f50858465eb9d68d20b2484b
arwahamdy <arwaomara32@gmail.com>
2025-12-11 22:33:24 +0200

    Add Auth Middleware

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('invoice', InvoiceController::class);
+Route::resource('invoice', InvoiceController::class)->middleware('auth');
